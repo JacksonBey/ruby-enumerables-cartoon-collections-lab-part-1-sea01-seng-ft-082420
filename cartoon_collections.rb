8269540d6cc04348ed0a88ec3e35d3c3f8c26fe7
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-06 17:19:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
 def greet_characters(array)
-  
+  array.each do |person|
+   puts "Hello #{person}!"
+end
+
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
