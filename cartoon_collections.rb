269b1e7ccb20658afda79538c6974ff795579791
Jackson Beytebiere <jacksongrowson@gmail.com>
2020-08-06 17:37:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,9 +2,12 @@ def greet_characters(array)
   array.each do |person|
    puts "Hello #{person}!"
 end
+end
 
 
 def list_dwarves(array)
   array.each_with_index do | person, index |
     puts "#{index}. #{person}"
 end
+end
+
